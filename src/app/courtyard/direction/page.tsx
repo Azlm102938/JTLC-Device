b6e6f8edfe5d2f7e64e0820eacef02947e1f5cee
Hugo Matthew Anwar (Peserta Praktik Kerja) <hugomatthew2803@gmail.com>
2025-05-23 12:44:48 +0700

    pop up update error

--- src/app/courtyard/direction/page.tsx
+++ src/app/courtyard/direction/page.tsx
@@ -1,6 +1,6 @@
 // app/direction/page.tsx
 'use client';
-import DirectionPage from "@/app/components/onProgress/page";
+import DirectionPage from "@/app/components/onProgress";
 
 export default function Page() {
   return <DirectionPage title="Direction" />;
